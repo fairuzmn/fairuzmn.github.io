a455cf89734cbe943b4cdcde3fdf05998ce7ee54
fairuzminannafis <fairuzminannafis@gmail.com>
2023-09-20 06:01:29 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d99cd73029262f5c067f.js"
+    "url": "webpack-runtime-ca39465db776413c97f0.js"
   },
   {
     "url": "framework-aa35e29fbb74d6100667.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "77556f00e9922233cc145d89f6df7901"
+    "revision": "848a5fab27cf1e03e771310bf57e8c65"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-50fcc746a3b9bd6c1224.js"
